b5e4b2844fee36429601680c7d46cb4e9d1ab3ba
olyanemyrovska <110451749+olyanemyrovska@users.noreply.github.com>
2024-08-04 21:57:44 +0200

    Homework_12_Maven

--- src/main/java/org/example/UserLoader.java
+++ src/main/java/org/example/UserLoader.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.lang.reflect.Type;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.example.dto.User;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
@@ -12,7 +18,14 @@ public class UserLoader {
         // з JSON файлу який знаходиться за відносним шляхом filepath
         // з допомогою вбудованих засобів бібліотеки https://mvnrepository.com/artifact/com.google.code.gson/gson/2.10.1
         // десеріалізує контент JSON файлу у список з об*єктів типу User і повертає список
-
-        return null;
+        Gson gson = new Gson();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        StringBuilder usersJson = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            usersJson.append(line);
+        }
+        Type collectionType = new TypeToken<List<User>>() {}.getType();
+        return gson.fromJson(String.valueOf(usersJson), collectionType);
     }
 }
